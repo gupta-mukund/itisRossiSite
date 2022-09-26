d6be8a4e3f98d00cdbafb45a81f56c3d1c6ff5d3
gupta-mukund <6190006@itisrossi.vi.it>
2022-09-26 21:58:44 +0200

    Update

--- script.js
+++ script.js
@@ -1,30 +1,82 @@
-// var currentImage = 0;
+var currentImage = 0;
+const sliderImages = [
+  {
+    image_url:
+      "https://www.itisrossi.edu.it/wp-content/uploads/2022/09/panoramica-istituto.jpg",
+    text: "La nostra scuola",
+    data: "12 settembre 2022",
+  },
+  {
+    image_url:
+      "https://www.itisrossi.edu.it/wp-content/uploads/2022/07/rossi-confindustria-panoramica.jpg",
+    text: "Evento Mind the Gap su iniziativa di Confindustria di Vicenza",
+    data: "17 luglio 2022",
+  },
+  {
+    image_url:
+      "https://www.itisrossi.edu.it/wp-content/uploads/2022/07/gnm-panoramica.jpg",
+    text: "Secondi alla Gara Nazionale di Meccanica",
+    data: "11 luglio 2022",
+  },
+  {
+    image_url:
+      "https://www.itisrossi.edu.it/wp-content/uploads/2022/06/nicola-home.jpg",
+    text: "Atletica, Nicola Testa entra in nazionale con la maglia azzurra",
+    data: "28 giugno 2022",
+  },
+  {
+    image_url:
+      "https://www.itisrossi.edu.it/wp-content/uploads/2022/06/foto-panoramica-2022.jpg",
+    text: "Buone vacanze e buoni esami!",
+    data: "17 giugno 2022",
+  },
+];
+const changeImage = () => {
+  document.getElementById("slider-image").src =
+    sliderImages[currentImage].image_url;
+  document.getElementById("sliderLink").innerText =
+    sliderImages[currentImage].text;
+  document.getElementById("sliderDate").innerText =
+    sliderImages[currentImage].data;
+  document.getElementById(currentImage + "").checked = true;
+};
+changeImage();
+const setImageByRadio = (e) => {
+  currentImage = e.target.id;
+  changeImage();
+};
 
-// const sliderImages = [
-//   "https://www.itisrossi.edu.it/wp-content/uploads/2022/09/panoramica-istituto.jpg",
-//   "https://www.itisrossi.edu.it/wp-content/uploads/2022/07/rossi-confindustria-panoramica.jpg",
-//   "https://www.itisrossi.edu.it/wp-content/uploads/2022/07/gnm-panoramica.jpg",
-//   "https://www.itisrossi.edu.it/wp-content/uploads/2022/06/nicola-home.jpg",
-//   "https://www.itisrossi.edu.it/wp-content/uploads/2022/06/foto-panoramica-2022.jpg",
-// ];
-// document.getElementById("image-container").style.backgroundImage =
-//   "url(" + sliderImages[currentImage] + ")";
-// const setImages = (e) => {
-//   if (e.currentTarget.myParam == -1 && currentImage == 0) {
-//     currentImage = 0;
-//   } else if (
-//     e.currentTarget.myParam == 1 &&
-//     currentImage == sliderImages.length - 1
-//   ) {
-//     currentImage = 0;
-//   } else {
-//     currentImage += e.currentTarget.myParam;
-//   }
-//   document.getElementById("image-container").style.backgroundImage =
-//     "url(" + sliderImages[currentImage] + ")";
-// };
+const radios = document.getElementsByName("sliderRadio");
+for (let radio of radios) {
+  radio.addEventListener("change", setImageByRadio);
+}
 
-// document.getElementById("left_arrow").addEventListener("click", setImages);
-// document.getElementById("left_arrow").myParam = -1;
-// document.getElementById("right_arrow").addEventListener("click", setImages);
-// document.getElementById("right_arrow").myParam = 1;
+const setImages = (e) => {
+  if (e.currentTarget.myParam == -1 && currentImage == 0) {
+    currentImage = 0;
+  } else if (
+    e.currentTarget.myParam == 1 &&
+    currentImage == sliderImages.length - 1
+  ) {
+    currentImage = 0;
+  } else {
+    currentImage += e.currentTarget.myParam;
+  }
+  changeImage();
+};
+
+document.getElementById("left_arrow").addEventListener("click", setImages);
+document.getElementById("left_arrow").myParam = -1;
+document.getElementById("right_arrow").addEventListener("click", setImages);
+document.getElementById("right_arrow").myParam = 1;
+
+const sliderAutoChange = () => {
+  if (currentImage == sliderImages.length - 1) {
+    currentImage = 0;
+  } else {
+    currentImage++;
+  }
+  changeImage();
+};
+
+//setInterval(sliderAutoChange, 2000);
